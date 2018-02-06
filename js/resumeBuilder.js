47a358f610e4ffd32d1fb24ca73bc6d2587639fd
guti7 <7505855+guti7@users.noreply.github.com>
2018-02-06 15:49:18 -0800

    Fix indentation format

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,12 +1,12 @@
 /*
 Your code to build the resume will go here.
- */
+*/
 
- let name = "Jose Gutierrez";
- let role = "Software Developer";
+let name = "Jose Gutierrez";
+let role = "Software Developer";
 
- let formattedName = HTMLheaderName.replace("%data%", name);
- let formattedRole = HTMLheaderRole.replace("%data%", role);
+let formattedName = HTMLheaderName.replace("%data%", name);
+let formattedRole = HTMLheaderRole.replace("%data%", role);
 
 // TODO: play with .prepend and indvidual calls once more content is added
- $("#header").prepend(formattedName, formattedRole);
+$("#header").prepend(formattedName, formattedRole);
