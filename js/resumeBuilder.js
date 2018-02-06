b693410cb30f6038f8b0767065bc04b4ad234137
guti7 <7505855+guti7@users.noreply.github.com>
2018-02-06 15:45:59 -0800

    Update page header

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,12 @@
 /*
-This is empty on purpose! Your code to build the resume will go here.
+Your code to build the resume will go here.
  */
+
+ let name = "Jose Gutierrez";
+ let role = "Software Developer";
+
+ let formattedName = HTMLheaderName.replace("%data%", name);
+ let formattedRole = HTMLheaderRole.replace("%data%", role);
+
+// TODO: play with .prepend and indvidual calls once more content is added
+ $("#header").prepend(formattedName, formattedRole);
