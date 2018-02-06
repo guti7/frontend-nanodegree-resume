5d49290e6d2d7f6bb2daabbf7cb285673c5ea4d4
guti7 <7505855+guti7@users.noreply.github.com>
2018-02-06 15:50:34 -0800

    Add education object

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -10,3 +10,25 @@ let formattedRole = HTMLheaderRole.replace("%data%", role);
 
 // TODO: play with .prepend and indvidual calls once more content is added
 $("#header").prepend(formattedName, formattedRole);
+
+var education = {
+  "schools": [{
+    "name": "Blue Ridge Community College",
+    "location": "Flat Rock",
+    "majors": ["Associate's in Science"],
+    "degreeDates": "2011",
+    "url": ""
+  }, {
+    "name": "NCSU",
+    "location": "Raleigh",
+    "majors": ["Computer Science"],
+    "degreeDates": "2014",
+    "url": ""
+  }],
+  "onlineCourses": [ {
+    "title": "FEND",
+    "school": "Udacity",
+    "dates": "February 2018",
+    "url": "https://udacity.com"
+  } ]
+}
