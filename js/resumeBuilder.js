19482ad2ee4cc875eeffaf50a2b1effdd2b55ed7
guti7 <7505855+guti7@users.noreply.github.com>
2018-02-06 17:34:09 -0800

    Format bio header using bio object

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,16 +1,6 @@
 /*
 Your code to build the resume will go here.
 */
-
-let name = "Jose Gutierrez";
-let role = "Software Developer";
-
-let formattedName = HTMLheaderName.replace("%data%", name);
-let formattedRole = HTMLheaderRole.replace("%data%", role);
-
-// TODO: play with .prepend and indvidual calls once more content is added
-$("#header").prepend(formattedName, formattedRole);
-
 var bio = {
   "name": "Jose Gutierrez",
   "role": "Software Developer",
@@ -77,3 +67,8 @@ var education = {
     "url": "https://udacity.com"
   }]
 };
+
+let formattedName = HTMLheaderName.replace("%data%", bio.name);
+let formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+
+$("#header").prepend(formattedName, formattedRole);
